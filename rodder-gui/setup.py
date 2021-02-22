33b2ad4dd5c6bce976a753528cf961d75d5d3415
Ruthenic <mdrakea3@tutanota.com>
2021-02-21 20:41:17 -0500

    remove template stuff from rodder-gui, install dependancies with rodder-gui

--- rodder-gui/setup.py
+++ rodder-gui/setup.py
@@ -1,6 +1,3 @@
-#PLEASE NOTE: THIS FORMAT IS NOT REQUIRED TO SUBMIT TO THE MAIN REPO, NOR IS IT REQUIRED FOR RODDER TO FUNCTION. THIS IS ONLY A TEMPLATE, USABLE WITH MOST PROGRAMS
-#Thanks, Drake.
-
 import subprocess,requests,zipfile,os,shutil
 dlurl = 'https://github.com/Ruthenic/rodder-gui/archive/master.zip' #this is where your download url 
 print(">< Downloading rodder-gui...")
@@ -10,13 +7,15 @@ with open(os.getenv('HOME') + '/.tmp/rodder/rodder-gui-master.zip', 'wb') as f:
 print(">< Extracting rodder-gui...")
 with zipfile.ZipFile(os.getenv('HOME') + '/.tmp/rodder/rodder-gui-master.zip', 'r') as f:
     f.extractall(os.getenv('HOME') + '/.tmp/rodder')
-print(">< Moving $PROGRAM_NAME to installation dir...")
+print(">< Moving rodder-gui to installation dir...")
 shutil.move(os.getenv('HOME') + '/.tmp/rodder/rodder-gui-master', os.getenv('HOME') + '/.local/bin')
-print(">< Adding $PROGRAM_NAME dir to $PATH")
+print(">< Adding rodder-gui dir to $PATH")
 with open(os.getenv('HOME') + '/.profile', 'a') as f:
     f.write('export PATH=' + os.getenv('HOME') + '/.local/bin/$EXTRACTED_FOLDER_NAME:$PATH #Created by rodder')
 if 'fish' in subprocess.run('echo "$SHELL"', shell=True, capture_output=True).stdout.decode('utf-8').strip():
     subprocess.run('/usr/bin/fish -c "set -Ua fish_user_paths {}/.local/bin/rodder-gui-master"'.format(os.getenv('HOME')), shell=True)
 print(">< Making rodder-gui executable...")
 subprocess.run('chmod +x {}/.local/bin/rodder-gui-master/rodder-gui'.format(os.getenv('HOME')), shell=True)
+print(">< Installing dependencies...")
+subprocess.run('python3 -m pip install -r {}/.local/bin/rodder-gui-master/requirements.txt'.format(os.getenv('HOME')), shell=True)
 print(">< Done!")
